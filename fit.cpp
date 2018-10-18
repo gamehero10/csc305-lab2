7d90455727cf6d688b90f0533382272be8921bac
Niko Lagman <Lagman.Niko@gmail.com>
2018-10-18 00:09:32 -0400

    Update Worst fit method

--- fit.cpp
+++ fit.cpp
@@ -11,7 +11,7 @@ public:
   static void firstFit (Jobs &, Partitions &);
   static int nextFit (Jobs &, Partitions &, int);
   static void bestFit (Jobs &, Partitions &);
-  static void worstFit (Jobs &, Partition &);
+  static void worstFit (Jobs &, Partitions &);
 };
 #endif
 
@@ -77,7 +77,7 @@ int Fit::nextFit (Jobs & jobs, Partitions & partitions, int nextPartition) {
 }
 
 void Fit::bestFit (Jobs & jobs, Partitions & partitions) {
-  
+
   // Iterate through jobs.
   for (int j = 0; j < jobs.size(); j++) {
 
@@ -91,7 +91,7 @@ void Fit::bestFit (Jobs & jobs, Partitions & partitions) {
       bool available = (partitions.get(i)->isFree());
       bool hasSpace = (partitions.get(i)->getSize() >= jobs.get(j)->getSize());
       bool betterFit = (best == -1) || (partitions.get(i)->getSize() < partitions.get(best)->getSize() );
-      if ( available && hasSpace && betterFit  ) {
+      if ( available && hasSpace && betterFit ) {
         
         // Keep track of best fitting partition.
         best = i;
@@ -114,33 +114,39 @@ void Fit::bestFit (Jobs & jobs, Partitions & partitions) {
 
 }
 
-void Fit::worstFit (Job * job, Partitions & partitions) {
-  int best = -1;
-  // Loop through partitions.
-  for (int i = 0; i < partitions.size(); i++) {
-    // If the current partition has enough storage to hold the job.
-    // And the current partition doesn't already have a job.
-    // And if the best fit index is either -1, or the current partition has greater
-    // difference than the previous partition. (better fit)
-    bool available = (partitions.get(i)->getJob() == -1);
-    bool hasSpace = (partitions.get(i)->getSize() >= job->getSize());
-    bool betterFit = (
-      (best == -1) ||
-      (partitions.get(i)->getSize() - job->getSize()) > (partitions.get(best)->getSize() - job->getSize())
-    );
-    if ( available && hasSpace && betterFit  ) {
-      best = i;
+void Fit::worstFit (Jobs & jobs, Partitions & partitions) {
+
+  // Iterate through jobs.
+  for (int j = 0; j < jobs.size(); j++) {
+
+    // Used to keep track of best partition fit for each job.
+    int best = -1;
+
+    // Iterate through through partitions.
+    for (int i = 0; i < partitions.size(); i++) {
+
+      // If the has enough space, is free, and the best fit is either -1 or is greater than the previous best fit.
+      bool available = (partitions.get(i)->isFree());
+      bool hasSpace = (partitions.get(i)->getSize() >= jobs.get(j)->getSize());
+      bool betterFit = (best == -1) || (partitions.get(i)->getSize() > partitions.get(best)->getSize() );
+      if ( available && hasSpace && betterFit ) {
+        
+        // Keep track of best fitting partition.
+        best = i;
+
+        // Break out of partitions iteration.
+        break;
+      }
     }
-  }
-  // If our best fit index is still -1, there was no available partition for this job.
-  if (best == -1) {
-    return;
-  }
-  // Else if there is a fit, link them.
-  else {
-    job->setPartition( partitions.get(best)->getNumber() );
-    job->setStatus(1);
-    partitions.get(best)->setJob( job->getNumber(), job->getSize() );
-    partitions.get(best)->setStatus(1);
+
+    // If there was a best fit.
+    if (best != -1) {
+      // Add reference of job to partition.
+      partitions.get(best)->setJob(jobs.get(j)->getName(), jobs.get(j)->getSize());
+
+      // Add reference of partition to job.
+      jobs.get(j)->setPartition(best);
+    }
+
   }
 }
